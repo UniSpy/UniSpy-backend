ada425be88adee668614a125dc9557ab730e730d
Fleuri <fleuri@Perkules.(none)>
2014-06-18 10:51:06 +0300

    Muutettu User modelia

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
     has_secure_password
     has_and_belongs_to_many :tags
+
+    validates :name, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true
 end
